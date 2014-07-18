b985a196601ea86291d5638ffb9033efc3e029a4
quno <qunoster@gmail.com>
2014-07-19 00:17:57 +0900

    update codes

--- src/morse.js
+++ src/morse.js
@@ -1,59 +1,100 @@
 //morse.js
 
-var btn = document.getElementById('myButton'),
-txt = document.getElementById('result'),
-set = document.getElementById('mySetting'),
-press_time = 0,
-press_start_time = 0,
-press_end_time = 0,
-blank_time = 0,
-blank_start_time = 0,
-blank_end_time = 0,
-press_setting = 200,
-blank_setting = 600;
+window.onload = function() {
+	var myButton = document.getElementById('myButton');
+	var mySetting = document.getElementById('mySetting');
+	var st = new SignalTimer();
 
-btn.addEventListener('mousedown',function(event){pressStart();});
-btn.addEventListener('mouseup',function(event){pressEnd();});
+	st.setOutput('result');
+	st.setSignalEventListener(myButton, ['mousedown', 'mouseup']);
+	st.setSignalEventListener(document, ['keydown', 'keyup']);
+	mySetting.addEventListener('mousedown',function(event) {
+		st.changeSetting(document.getElementById('short').value,document.getElementById('blank').value);
+	});
+};
 
-set.addEventListener('mousedown',function(event){
-	var new_press_setting = document.getElementById('short').value,
-	new_blank_setting = document.getElementById('blank').value;
-	if(new_press_setting !== undefined && new_blank_setting !== undefined){
-		press_setting = new_press_setting;
-		blank_setting = new_blank_setting;
-		console.log('new setting:' + press_setting + ' / ' + blank_setting);
-	}
-});
-
-document.addEventListener('keydown',function(event){
-	if(event.keyCode == 32){
-		pressStart();
-	}
-});
-document.addEventListener('keyup',function(event){
-	if(event.keyCode == 32){
-		pressEnd();
-	}
-});
-
-function pressStart(){
-	press_start_time = blank_end_time = new Date();
-	if(blank_start_time) {
-		blank_time = blank_end_time - blank_start_time;
-		if(blank_time > blank_setting) {
-			txt.value += " / ";
+//モールス信号管理クラス
+function SignalTimer() {
+	this.press = {
+		time: 0,
+		start: 0,
+		end: 0
+	};
+	this.blank = {
+		time: 0,
+		start: 0,
+		end: 0
+	};
+	this.setting = {
+		press: 200,
+		blank: 600
+	};
+	this.output = {};
+}
+//押してる時間の取得・処理
+SignalTimer.prototype.setPressTime = function(event) {
+	if(this.isDown(event)) {
+		this.press.start = new Date();
+	} else if(this.isUp(event)) {
+		this.press.end = new Date();
+		this.press.time = this.press.end - this.press.start;
+		//短点・長点の判断
+		if(this.press.time < this.setting.press) {
+			this.output.value += "・";
+		} else {
+			this.output.value += "―";
 		}
-		console.log('blank:' + blank_time + 'ms');
-		blank_start_time = 0;
+		console.log('press:' + this.press.time + 'ms');
 	}
-}
-function pressEnd(){
-	press_end_time = blank_start_time = new Date();
-	press_time = press_end_time - press_start_time;
-	if(press_time < press_setting) {
-		txt.value += "・";
+};
+//押してない時間の取得・処理
+SignalTimer.prototype.setBlankTime = function() {
+	if(this.isDown(event)) {
+		this.blank.end = new Date();
+		if(this.blank.start > 0) {
+			this.blank.time = this.blank.end - this.blank.start;
+			//文字区切りの判断
+			if(this.blank.time > this.setting.blank) {
+				this.output.value += " / ";
+			}
+			console.log('blank:' + this.blank.time + 'ms');
+			this.blank.start = 0;
+		}
+	} else if(this.isUp(event)) {
+		this.blank.start = new Date();
+	}
+};
+//しきい値設定の変更
+SignalTimer.prototype.changeSetting = function(press_setting, blank_setting) {
+	if(typeof press_setting !== undefined && typeof blank_setting !== undefined) {
+		this.setting.press = press_setting;
+		this.setting.blank = blank_setting;
+		console.log('change setting:' + this.setting.press + 'ms / ' + this.setting.blank + 'ms');
 	} else {
-		txt.value += "―";
+		console.error('wrong setting');
 	}
-	console.log('press:' + press_time + 'ms');
-}
+};
+//マウスか特定のキーが押されたかの判断
+SignalTimer.prototype.isDown = function(event) {
+	return ((event.type ===  'keydown' && event.keyCode === 32) ||
+			event.type === 'mousedown');
+};
+//マウスか特定のキーが離されたかの判断
+SignalTimer.prototype.isUp = function(event) {
+	return ((event.type === 'keyup' && event.keyCode === 32) ||
+			event.type === 'mouseup');
+};
+//出力場所の設定
+SignalTimer.prototype.setOutput = function(id) {
+	this.output = document.getElementById(id);
+};
+//イベントリスナーの設定		
+SignalTimer.prototype.setSignalEventListener = function(element, events) {
+	var that = this;
+	events.forEach(function(event_name) {
+		element.addEventListener(event_name,function(event) {
+			that.setPressTime(event);
+			that.setBlankTime(event);
+		});
+	});
+};
